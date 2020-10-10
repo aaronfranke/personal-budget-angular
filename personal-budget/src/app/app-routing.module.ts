04660566631feaae8e3d740495547ee0de6b5f20
Aaron Franke <arnfranke@yahoo.com>
2020-10-09 23:33:00 -0400

    Complete changes in Coding Exercise 02

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 import { P404Component } from './p404/p404.component';
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
 	{
 		path: "login",
 		component: LoginComponent
+	},
+	{
+		path: "contact",
+		component: ContactComponent
 	},
 	{
 		path: "**",
